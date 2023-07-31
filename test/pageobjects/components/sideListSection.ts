044821e35b32a9cba32e9c27926b263f1f290bab
Valeriia Parfelyuk <parfelyuk.v@gmail.com>
2023-07-31 17:12:17 +0200

    AQA-15 fix test add List to favorites

--- test/pageobjects/components/sideListSection.ts
+++ test/pageobjects/components/sideListSection.ts
@@ -1,16 +1,31 @@
+import addList from "./addList.js";
 
 class SideListSection {
-    public get buttonFavoriteList () {
+    public get buttonFavoriteList() {
         return $('.menu.namespaces-lists .favorite');
     }
 
-    public get listItem () {
+    public get listItem() {
         return $('.list-menu-title');
     }
 
-    public get dropdownMenu () {
+    public get dropdownMenu() {
         return $('button.dropdown-trigger');
     }
-} 
+
+    public get newList() {
+        return $$('.dropdown-item')[2];
+    }
+
+    public get expectedList() {
+        return $('.menu-list.can-be-hidden.dragging-disabled');
+    }
+
+    public async addNewList(listTitle) {
+        await this.dropdownMenu.click();
+        await this.newList.click();
+        await addList.addNewlist(listTitle);
+    }
+}
 
 export default new SideListSection();
--- test/specs/lists/addListToFavorites.ts
+++ test/specs/lists/addListToFavorites.ts
@@ -1,25 +1,28 @@
-import {signInWithCredsApi} from '../../businessFunctions/loginAPI.js';
-import { signInWithCredsUi } from '../../businessFunctions/loginUI.js';
 import sideListSection from '../../pageobjects/components/sideListSection.js';
 import favoritesSection from '../../pageobjects/components/favoritesSection.js';
+import overviewPage from '../../pageobjects/overview.page.js';
+import { createTitleForList } from '../../utils/titles.js';
+import { createUserWithSignInApi } from '../../businessFunctions/loginAPI.js';
 
-describe('User should be able to',  () => {
+
+describe('User should be able to', () => {
     it('add the list to favorites using star button on overview page', async () => {
         //sign in
-        // await signInWithCredsUi();
-        await signInWithCredsApi({username: 'test_4', password: '123'});
+        await createUserWithSignInApi();
+
+        // creare list of tlists
+        await overviewPage.addNewlist(createTitleForList());
+        await sideListSection.addNewList(createTitleForList());
+        await sideListSection.addNewList(createTitleForList());
 
         //selected the first list from the menu of lists and chose as favorite
         const locatorFavoriteList = await sideListSection.buttonFavoriteList.parentElement();
-        const foo = $('.menu-list.can-be-hidden.dragging-disabled');
         await sideListSection.buttonFavoriteList.click();
         //check the favorite list in the special section is appeared
 
         await expect(favoritesSection.headerSection).toBeDisplayed();
         await expect(favoritesSection.starFavoriteButton).toBeDisplayed();
-        await expect(locatorFavoriteList).toHaveText(await foo.getText());
+        await expect(locatorFavoriteList).toHaveText(await sideListSection.expectedList.getText());
 
     })
-
-    it('add list to favorites on Namespaces & Lists page')
 })
